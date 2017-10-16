fe70e5de7c7ac59b0e588a6c6e47b429070a7864
Tira <217bssr@gmail.com>
2017-10-16 13:57:55 +0000

    課題3.2　機能自体のの実装完了

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,20 +3,20 @@ class UserController < ApplicationController
     @userFlg = current_user.admin_flg
     if @userFlg ==  true
       @userInfo = User.find_by_sql(['select id,email,locked_at from users'])
-    else
+    else lo
       redirect_to dashboard_index_path
     end
   end
   
   def changeAccountStatus
     email = params[:email]
-    user = User.find_by_sql(["SELECT * FROM users WHERE email='#{email}'"])
-    if user.locked_at.nil?
+    locked_at = ActiveRecord::Base.connection.select_all("SELECT locked_at FROM users WHERE email='#{email}'").to_a[0]["locked_at"]
+    if locked_at.nil?
       require 'date'
       time = DateTime.now
-      User.find_by_sql(["UPDATE users SET	locked_at = '#{time}' WHERE email='#{email}'"])    
+      ActiveRecord::Base.connection.select_all("UPDATE users SET locked_at = '#{time}' WHERE email='#{email}'")
     else
-      User.find_by_sql(["UPDATE users SET	locked_at = nil WHERE email='#{email}'"])    
+      ActiveRecord::Base.connection.select_all("UPDATE users SET locked_at = null WHERE email='#{email}'")
     end
     redirect_to user_index_path
   end
